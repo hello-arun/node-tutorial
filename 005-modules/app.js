4600a48450ce46b9bd39d7983c5b0921a25b6729
Arun <jangirarun786@gmail.com>
2022-10-08 15:36:23 +0300

    05-modules added

---
+++ 005-modules/app.js
@@ -0,0 +1,9 @@
+const tutorial = require("./tutorial")  // No need to path externsio
+console.log(tutorial)
+
+console.log(tutorial.sum(10,20))
+console.log(tutorial.PI)
+console.log(new tutorial.SomeMathObject())
+
+
+// console.log(tutorial(2,2))
