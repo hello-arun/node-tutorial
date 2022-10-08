8ddbb50b252bb65b50584d0ffaa8e9292ecb5699
Arun <jangirarun786@gmail.com>
2022-10-08 17:02:07 +0300

    http module url based respose added

--- 035-http-module/app.js
+++ 035-http-module/app.js
@@ -1,12 +1,17 @@
 const http = require("http");
 const PORT = 3000;
 const server = http.createServer((req, res) => {
-    res.write("Hello World from node.js");
-    res.end();
+    if (req.url === "/") {
+        res.write("Hello World from node.js");
+        res.end();
+    } else {
+        res.write("Using some other domain");
+        res.end();
+    }
 });
 
 // Port number to listen from
 server.listen(PORT);
 
 // Help Message
-console.log(`Open in google chrome\nhttp://localhost:${PORT}`);
+console.log(`Open in google chrome\nhttp://localhost:${PORT}\n or \nhttp://localhost:${PORT}/banana`);
