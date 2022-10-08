6759be1c3e847acd5731d7546c19f488e50b99a2
Arun <jangirarun786@gmail.com>
2022-10-08 16:19:13 +0300

    read write updated minot

--- 020-filesystem-module/01-read-write.js
+++ 020-filesystem-module/01-read-write.js
@@ -4,7 +4,7 @@ fs.writeFile("example.txt", "This is an example", (err) => {
     else {
         console.log("File successfully created!");
         // Encoding is necessary to read the fil
-        fs.readFile("example.txt", 'utf8',(err, file) => {
+        fs.readFile("example.txt", "utf8", (err, file) => {
             if (err) console.log(err);
             else console.log(file);
         });
