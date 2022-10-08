2016c72779d8194b07bf1092da2d6ea88b76fbca
Arun <jangirarun786@gmail.com>
2022-10-08 16:11:45 +0300

    filesystem with encoding

--- 020-filesystem-module/app.js
+++ 020-filesystem-module/app.js
@@ -3,7 +3,8 @@ fs.writeFile("example.txt", "This is an example", (err) => {
     if (err) console.log(err);
     else {
         console.log("File successfully created!");
-        fs.readFile("example.txt", (err, file) => {
+        // Encoding is necessary to read the fil
+        fs.readFile("example.txt", 'utf8',(err, file) => {
             if (err) console.log(err);
             else console.log(file);
         });
